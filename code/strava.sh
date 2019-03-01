ac20f9ce148ac8559daaf9a93989d6ed064d9072
Brendan Apfeld <brendan.apfeld@utexas.edu>
2019-03-01 16:27:07 -0600

    change command line to current options

--- code/strava.sh
+++ code/strava.sh
@@ -3,5 +3,4 @@
 python3 strava.py \
         --db_file='~/strava/data/strava.db' \
         --rider_name='Brendan' \
-        --ini_path='~/strava/code/strava.ini' \
-        --schema_path='~/strava/code/create_db.sql'
+        --secrets_path='~/strava/code/strava.gpg'
