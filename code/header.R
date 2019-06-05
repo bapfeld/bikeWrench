782ab98649bf4eb66414e7e5b15c11f07081bdb3
Brendan Apfeld <brendan.apfeld@utexas.edu>
2019-06-04 20:45:47 -0500

    delete useless code

--- code/header.R
+++
@@ -1,9 +0,0 @@
-# header file for project
-setwd("~/strava")
-
-suppressMessages(library(dplyr))
-suppressMessages(library(ggplot2))
-library(tidyr)
-library(strava)
-
-print("Packages loaded: dplyr, tidyr, ggplot2, strava")
