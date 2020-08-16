f80b9542410418ad88a4cf7109ed82c5fd1d515a
Brendan Apfeld <brendan.apfeld@utexas.edu>
2020-08-16 16:16:42 -0500

    adds basic setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='bikeGarage',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Tool to manage bicycles and bike parts',
+    author='Brendan Apfeld',
+)
