406e5c8635366accbdc69adbc62cce6868c29870
Brendan Apfeld <brendan.apfeld@utexas.edu>
2021-12-29 11:35:06 -0600

    moves all config to config.py

--- app/config.py
+++ app/config.py
@@ -1,9 +1,10 @@
 import os
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 import keyring
 
-# basedir = os.path.abspath(os.path.dirname(__file__))
-# load_dotenv(os.path.join(basedir, '.env'))
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
 
 class Config:
     """Basic/base configuration"""
@@ -12,6 +13,15 @@ class Config:
     WTF_CSRF_ENABLED = False
     STATIC_FOLDER = 'static'
     TEMPLATES_FOLDER = 'templates'
+
+    # Database
+    DB_PATH = os.environ.get('STRAVA_DB_PATH')
+    SCHEMA_PATH = os.environ.get('SCHEMA_PATH')
+
+    # Strava secrets
+    CLIENT_ID = os.environ.get('STRAVA_CLIENT_ID')
+    CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
+    APP_CODE = keyring.get_password('bikeWrench', 'code')
 
 
 class DevConfig(Config):
