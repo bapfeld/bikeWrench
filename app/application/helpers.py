a1906effe1a16baa17100ff047eba728a7fa57fb
Brendan Apfeld <brendan.apfeld@utexas.edu>
2021-12-29 14:34:12 -0600

    moves some helper funcs to helpers

--- app/application/helpers.py
+++ app/application/helpers.py
@@ -8,3 +8,87 @@ def units_text(unit_type):
         dist_unit = 'kilometers'
         elev_unit = 'meters'
     return (speed_unit, dist_unit, elev_unit)
+
+
+def convert_rider_info(units, max_speed, avg_speed, tot_dist, tot_climb):
+    if units == 'imperial':
+        max_speed = round(max_speed / 1.609, 2)
+        avg_speed = round(avg_speed / 1.609, 2)
+        tot_dist = round(tot_dist / 1.609, 2)
+        tot_climb = round(tot_climb * 3.281, 2)
+    else:
+        max_speed = round(max_speed, 2)
+        avg_speed = round(avg_speed, 2)
+        tot_dist = round(tot_dist, 2)
+        tot_climb = round(tot_climb, 2)
+    return (max_speed, avg_speed, tot_dist, tot_climb)
+
+
+def none_or_zero(z):
+    if z:
+        return 0
+
+
+def convert_summary_units(units, res, zeros=False):
+    if units == 'imperial':
+        try:
+            dist = round(res[0] / 1.609, 2)
+        except TypeError:
+            dist = none_or_zero(zeros)
+        try:
+            climb = round(res[3] * 3.281, 2)
+        except TypeError:
+            climb = none_or_zero(zeros)
+        try:
+            mx_speed = round(res[6] / 1.609, 2)
+        except TypeError:
+            mx_speed = none_or_zero(zeros)
+    else:
+        try:
+            dist = round(res[0], 2)
+        except TypeError:
+            dist = none_or_zero(zeros)
+        try:
+            climb = round(res[3], 2)
+        except TypeError:
+            climb = none_or_zero(zeros)
+        try:
+            mx_speed = round(res[6], 2)
+        except TypeError:
+            mx_speed = none_or_zero(zeros)
+    try:
+        mv_time = round(res[4], 1)
+    except TypeError:
+        mv_time = none_or_zero(zeros)
+    try:
+        s_time = round(res[5], 1)
+    except TypeError:
+        s_time = none_or_zero(zeros)
+    try:
+        cal = round(res[7], 2)
+    except TypeError:
+        cal = none_or_zero(zeros)
+    return (dist, res[1], res[2], climb, mv_time, s_time, mx_speed, cal)
+
+
+def combine_res(n_all, n_virt):
+    return (round(n_all - n_virt, 1), n_virt, n_all)
+
+
+def summary_stats_combo(r_all, r_virt, u):
+    r_all = convert_summary_units(u, r_all)
+    r_virt = convert_summary_units(u, r_virt, zeros=True)
+    out = dict()
+    out['dist'] = combine_res(r_all[0], r_virt[0])
+    try:
+        out['min_dt'] = min([r_all[1], r_virt[1]])
+        out['max_dt'] = max([r_all[2], r_virt[2]])
+    except TypeError:
+        out['min_dt'] = r_all[1]
+        out['max_dt'] = r_all[2]
+    out['elev'] = combine_res(r_all[3], r_virt[3])
+    out['moving_time'] = combine_res(r_all[4], r_virt[4])
+    out['elapsed_time'] = combine_res(r_all[5], r_virt[5])
+    out['max_speed'] = (r_all[6], r_virt[6])
+    out['cal'] = combine_res(r_all[7], r_virt[7])
+    return out
